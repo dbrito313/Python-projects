9941d1095729153684f6a10143c30115882422d1
dbrito313 <danielbrito.league@gmail.com>
2025-01-30 17:22:00 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import game_data
+
+
+a_or_b = input("Who has more follower? Type 'A' or 'B': ").lower()
+
+
+
+choices = print(f"Compare A:
+score = 0
+
+
+def scoring():
+    global score
+    if a_or_b == "a" and A > B:
+        score += 1
+        print(f"You're right! Current score : {score}")
